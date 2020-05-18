4e3fdfeb1987e51c13c16b89034d6e35ce015fe3
pat-liu <liupat@seas.upenn.edu>
2020-05-17 17:41:28 -0700

    Updates

---
+++ precache-manifest.dc5ceca24ea8e550ce7e9bf18df2a0d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0aca633935102c0f933f4cd5a8ec33",
+    "url": "/reddit-word-cloud/index.html"
+  },
+  {
+    "revision": "e4aad86751c12783ac55",
+    "url": "/reddit-word-cloud/static/css/main.6931bea6.chunk.css"
+  },
+  {
+    "revision": "6b8f28dd61be98c2173e",
+    "url": "/reddit-word-cloud/static/js/2.392560f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddit-word-cloud/static/js/2.392560f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4aad86751c12783ac55",
+    "url": "/reddit-word-cloud/static/js/main.19cd18d7.chunk.js"
+  },
+  {
+    "revision": "ec367145994bf6850d19",
+    "url": "/reddit-word-cloud/static/js/runtime-main.917d1736.js"
+  },
+  {
+    "revision": "3d50e7f9b0e42f69dd2060572c543ef4",
+    "url": "/reddit-word-cloud/static/media/logo.3d50e7f9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddit-word-cloud/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reddit-word-cloud/precache-manifest.dc5ceca24ea8e550ce7e9bf18df2a0d1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reddit-word-cloud/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
